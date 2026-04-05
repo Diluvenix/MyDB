48e48b7a8b2c9e4f834663f4dfcbfc7e5d8afcbe
DrKeks1010 <jonas.putz@icloud.com>
2026-04-05 14:57:35 +0200

    Adds node child split constants

--- include/const.h
+++ include/const.h
@@ -12,7 +12,9 @@ typedef uint64_t page64_t;
 #define TABLE_NAME_LENGTH_MIN   (3)
 #define TABLE_NAME_LENGTH_MAX   ((PAGE_SIZE) - (6 * 8))
 
-#define TABLE_NODE_CHILD_COUNT  (((PAGE_SIZE) - (6 * 8)) / 16)
-#define TABLE_NODE_IS_LEAF_FLAG (1 << 0)
+#define TABLE_NODE_CHILD_COUNT      (((PAGE_SIZE) - (6 * 8)) / 16)
+#define TABLE_NODE_CHILD_SPLIT_MAX  (((TABLE_NODE_CHILD_COUNT) + 1) / 2)
+#define TABLE_NODE_CHILD_SPLIT_MIN  ((TABLE_NODE_CHILD_COUNT) + 1 - (TABLE_NODE_CHILD_SPLIT_MAX))
+#define TABLE_NODE_IS_LEAF_FLAG     (1 << 0)
 
 #endif
